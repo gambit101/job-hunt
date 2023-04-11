e171ee33f8e902a4ed56c8fa57b0e27502d71737
gambit101 <mohammadsamraj@gmail.com>
2023-04-11 14:48:32 +0600

    added a component

--- src/components/AppliedJobs.jsx
+++ src/components/AppliedJobs.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { getStoredCart } from '../utils/fakeDB';
 import { useLoaderData } from 'react-router-dom';
+import SingleJob from './SingleJob';
 
 const AppliedJobs = () => {
     const [cart, setCart] = useState([]);
@@ -23,32 +24,7 @@ const AppliedJobs = () => {
     return (
         <div>
             {
-                cart.map(item => <div className='flex justify-center mt-10 border-2 w-2/3 ml-44 p-4 rounded-md'>
-                    <img className='h-36 w-34 mx-6rounded-md' src={item.company_logo} alt="" />
-                    <div className='flex justify-between mx-6'>
-                        <div>
-                            <p>{item.title}</p>
-                            <p>{item.company_name}</p>
-                            <div className='flex mt-2'>
-                                <button className='border-2 border-cyan-600 p-1 rounded hover:bg-cyan-400'>Remote</button>
-                                <button className='border-2 border-cyan-600 p-1 rounded hover:bg-cyan-400 ml-2'>Full Time</button>
-                            </div>
-                            <div className='flex'>
-                                <p className='text-gray-600 mt-2'>
-                                    {item.location}
-                                </p>
-                                <p className='text-gray-600 mt-2 ml-3' >
-                                    {item.salary}
-                                </p>
-                            </div>
-
-                        </div>
-                        <div className='ml-24 mt-8'>
-                            <button className='btn-primary'>View Details</button>
-                        </div>
-                    </div>
-
-                </div>)
+                cart.map(item => <SingleJob key={item.id} item={item}></SingleJob>)
             }
         </div>
     );
