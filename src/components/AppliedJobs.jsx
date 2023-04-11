6b2940c61221a1562c8620cc39356e67c5d4484d
gambit101 <mohammadsamraj@gmail.com>
2023-04-11 14:24:25 +0600

    applied job page done

--- src/components/AppliedJobs.jsx
+++ src/components/AppliedJobs.jsx
@@ -23,7 +23,32 @@ const AppliedJobs = () => {
     return (
         <div>
             {
-                cart.map(item=><p key={item.id}>{item.title}</p>)
+                cart.map(item => <div className='flex justify-center mt-10 border-2 w-2/3 ml-44 p-4 rounded-md'>
+                    <img className='h-36 w-34 mx-6rounded-md' src={item.company_logo} alt="" />
+                    <div className='flex justify-between mx-6'>
+                        <div>
+                            <p>{item.title}</p>
+                            <p>{item.company_name}</p>
+                            <div className='flex mt-2'>
+                                <button className='border-2 border-cyan-600 p-1 rounded hover:bg-cyan-400'>Remote</button>
+                                <button className='border-2 border-cyan-600 p-1 rounded hover:bg-cyan-400 ml-2'>Full Time</button>
+                            </div>
+                            <div className='flex'>
+                                <p className='text-gray-600 mt-2'>
+                                    {item.location}
+                                </p>
+                                <p className='text-gray-600 mt-2 ml-3' >
+                                    {item.salary}
+                                </p>
+                            </div>
+
+                        </div>
+                        <div className='ml-24 mt-8'>
+                            <button className='btn-primary'>View Details</button>
+                        </div>
+                    </div>
+
+                </div>)
             }
         </div>
     );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,15 +4,17 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <div className='flex justify-evenly my-6'>
-            <div className='text-3xl font-bold tracking-wide text-gray-800'>
-                JobHunt
-            </div>
+            <Link to='/'>
+                <div className='text-3xl font-bold tracking-wide text-gray-800'>
+                    JobHunt
+                </div>
+            </Link>
             <ul className='flex gap-8'>
                 <Link className='mt-2 hidden space-x-8 lg:flex font-bold' to='/'>Home</Link>
-                <Link className='mt-2 hidden space-x-8 lg:flex font-bold'  to='/stats'>Stats</Link>
-                <Link className='mt-2 hidden space-x-8 lg:flex font-bold'  to='/appliedJobs'>Applied Jobs</Link>
-                <Link className='mt-2 hidden space-x-8 lg:flex font-bold'  to='/blog'>Blog</Link>
-                
+                <Link className='mt-2 hidden space-x-8 lg:flex font-bold' to='/stats'>Stats</Link>
+                <Link className='mt-2 hidden space-x-8 lg:flex font-bold' to='/appliedJobs'>Applied Jobs</Link>
+                <Link className='mt-2 hidden space-x-8 lg:flex font-bold' to='/blog'>Blog</Link>
+
             </ul>
             <div>
                 <button className='btn-primary'>Start Applying</button>
